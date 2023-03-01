847588bf65886037ea3faa7776d78c0cc53bb542
P4anix <pancok_1@yahoo.it>
2023-03-01 18:56:20 +0100

    some fix

--- Pari e dispari/main.js
+++ Pari e dispari/main.js
@@ -1,20 +1,12 @@
 'Use strict'
+/*---------- 
+  Function
+ ----------*/
 
-/* chiedo all'utente di scegliere pari o dispari */
-let sceltaUtente = prompt("Scegli pari o dispari:");
-
-/* chiedo all'utente di inserire un numero da 1 a 5 */
-let numeroUtente = Number(prompt("Inserisci un numero da 1 a 5:"));
-
-/* funzione per numero casuale */
+ /* funzione per numero casuale */
 function numeroRandomico(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-let numeroComputer = numeroRandomico(1,5);
-
-/* sommo i due numeri */
-let somma = numeroUtente + numeroComputer;
 
 /* definisco una funzione per stabilire se la somma è pari o dispari */
 function maybe(numero) {
@@ -22,12 +14,29 @@ function maybe(numero) {
   return numero % 2 === 0;
 }
 
+/*---------- 
+    Main
+ ----------*/
+
+/* chiedo all'utente di scegliere pari o dispari */
+let sceltaUtente = prompt("Scegli pari o dispari:");
+
+/* chiedo all'utente di inserire un numero da 1 a 5 */
+let numeroUtente = Number(prompt("Inserisci un numero da 1 a 5:"));
+
+let numeroComputer = numeroRandomico(1,5);
+
+/* sommo i due numeri */
+let somma = numeroUtente + numeroComputer;
+
 /* controllo se la somma è pari o dispari */
 let sommaPari = maybe(somma);
 
 // controllo chi ha vinto
 if ((sceltaUtente === "pari" && sommaPari) || (sceltaUtente === "dispari" && !sommaPari)) {
     console.log("Hai vinto! Il tuo numero è " + numeroUtente + ", il numero del computer è " + numeroComputer + ", la somma è " + somma );
+    alert("Hai vinto! Il tuo numero è " + numeroUtente + ", il numero del computer è " + numeroComputer + ", la somma è " + somma );
   } else {
     console.log("Hai perso! Il tuo numero è " + numeroUtente + ", il numero del computer è " + numeroComputer + ", la somma è " + somma );
+    alert("Hai perso! Il tuo numero è " + numeroUtente + ", il numero del computer è " + numeroComputer + ", la somma è " + somma );
   }
