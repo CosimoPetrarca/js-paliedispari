1cf25cec142658a194f1a3e22f106e1761d61b95
P4anix <pancok_1@yahoo.it>
2023-03-01 17:54:03 +0100

    pari o dispari done

--- Pari e dispari/main.js
+++ Pari e dispari/main.js
@@ -1,1 +1,29 @@
 'Use strict'
+
+/* chiedo all'utente di scegliere pari o dispari */
+let sceltaUtente = prompt("Scegli pari o dispari:");
+
+/* chiedo all'utente di inserire un numero da 1 a 5 */
+let numeroUtente = Number(prompt("Inserisci un numero da 1 a 5:"));
+
+/* genero un numero casuale per il computer da 1 a 5 */
+let numeroComputer = Math.floor(Math.random() * 5) + 1;
+
+/* sommo i due numeri */
+let somma = numeroUtente + numeroComputer;
+
+/* definisco una funzione per stabilire se la somma è pari o dispari */
+function maybe(numero) {
+
+  return numero % 2 === 0;
+}
+
+/* controllo se la somma è pari o dispari */
+let sommaPari = maybe(somma);
+
+// controllo chi ha vinto
+if ((sceltaUtente === "pari" && sommaPari) || (sceltaUtente === "dispari" && !sommaPari)) {
+    console.log("Hai vinto! Il tuo numero è " + numeroUtente + ", il numero del computer è " + numeroComputer + ", la somma è " + somma );
+  } else {
+    console.log("Hai perso! Il tuo numero è " + numeroUtente + ", il numero del computer è " + numeroComputer + ", la somma è " + somma );
+  }
